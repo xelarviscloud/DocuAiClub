5ed31b463cdbfba30a621ae0782708f271cb780a
HG <ghori_hitesh@yahoo.com>
2024-03-17 13:09:32 -0500

    fix(loginpage)

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -12,13 +12,16 @@ import {
   CInputGroup,
   CInputGroupText,
   CRow,
+  CFormLabel,
+  CCardImage,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
 import { useNavigate } from 'react-router-dom'
 import { userLogin } from '../../../services/LoginService'
 import setCookie from '../../../resources/utility'
-
+import Background from '../../../assets/login-bg.jpg'
+import Logo from '../../../assets/logo-bg-1.png'
 const Login = () => {
   const navigate = useNavigate()
 
@@ -38,114 +41,74 @@ const Login = () => {
   }
   return (
     <CRow>
-      <CCol xs={5} className="min-vh-100 d-flex flex-row align-items-center justify-content-center">
-        <div>
-          <CCard>
-            <CCardBody>
-              <CForm>
-                <h1>Login</h1>
-                <p className="text-body-secondary">Sign In to your account</p>
-                <CInputGroup className="mb-3">
-                  <CInputGroupText>
+      <CCol xs={5} className="min-vh-100 d-flex flex-row  justify-content-center">
+        <div style={{ position: 'relative' }} className="align-items-center">
+          <CCardImage
+            orientation="top"
+            // className="coin"
+            src={Logo}
+            style={{
+              borderRadius: '100%',
+              width: 120,
+              left: '19%',
+              top: 270,
+            }}
+          />
+          <h1>DocuAi Club</h1>
+          <h5>Restaging Documents</h5>
+          <CForm style={{ marginTop: 60 }}>
+            <h3 className="text-body-secondary">Sign In.</h3>
+
+            <div className="mb-3">
+              {/* <CInputGroupText>
                     <CIcon icon={cilUser} />
-                  </CInputGroupText>
-                  <CFormInput size="lg" placeholder="Username" autoComplete="username" />
-                </CInputGroup>
-                <CInputGroup className="mb-4">
-                  <CInputGroupText>
+                  </CInputGroupText> */}
+              <CFormLabel htmlFor="Username">User Name</CFormLabel>
+              <CFormInput size="lg" placeholder="Username" autoComplete="username" />
+            </div>
+            <div className="mb-3">
+              {/* <CInputGroupText>
                     <CIcon icon={cilLockLocked} />
-                  </CInputGroupText>
-                  <CFormInput
-                    size="lg"
-                    type="password"
-                    placeholder="Password"
-                    autoComplete="current-password"
-                  />
-                </CInputGroup>
-                <CRow>
-                  <CCol xs={6}>
-                    <CButton color="success" className="px-4" onClick={(e) => handleLogin(e)}>
-                      Login
-                    </CButton>
-                  </CCol>
-                  <CCol xs={6} className="text-right">
-                    <CButton color="link" className="px-0">
-                      Forgot password?
-                    </CButton>
-                  </CCol>
-                </CRow>
-              </CForm>
-            </CCardBody>
-          </CCard>
+                  </CInputGroupText> */}
+              <CFormLabel htmlFor="Password">Password</CFormLabel>
+              <CFormInput
+                size="lg"
+                type="password"
+                placeholder="Password"
+                autoComplete="current-password"
+              />
+            </div>
+            <CRow>
+              <CCol xs={6}>
+                <CButton color="primary" className="px-4" onClick={(e) => handleLogin(e)}>
+                  Login
+                </CButton>
+              </CCol>
+              <CCol xs={6} className="text-right">
+                <CButton color="link" className="px-0">
+                  Forgot password?
+                </CButton>
+              </CCol>
+            </CRow>
+          </CForm>
         </div>
       </CCol>
-      <CCol xs={7} style={{ backgroundColor: '#e5eff7' }}></CCol>
+      <CCol
+        xs={7}
+        style={{
+          backgroundImage: `url(${Background})`,
+          backgroundColor: '#ffffff99',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          opacity: 0.9,
+        }}
+        className="min-vh-100 d-flex flex-row align-items-center justify-content-center"
+      >
+        <div style={{ opacity: 1 }}>
+          {/* <h1 style={{ color: '#222222' }}>Restaging Document Storage</h1> */}
+        </div>
+      </CCol>
     </CRow>
-    // <div
-    //   className="min-vh-100 d-flex flex-row align-items-center"
-    //   style={{ backgroundColor: 'white' }}
-    // >
-    //   <CContainer>
-    //     <CRow>
-    //       <CCol xs={4}>
-    //         <CCard>
-    //           <CCardBody>
-    //             <CForm>
-    //               <h1>Login</h1>
-    //               <p className="text-body-secondary">Sign In to your account</p>
-    //               <CInputGroup className="mb-3">
-    //                 <CInputGroupText>
-    //                   <CIcon icon={cilUser} />
-    //                 </CInputGroupText>
-    //                 <CFormInput placeholder="Username" autoComplete="username" />
-    //               </CInputGroup>
-    //               <CInputGroup className="mb-4">
-    //                 <CInputGroupText>
-    //                   <CIcon icon={cilLockLocked} />
-    //                 </CInputGroupText>
-    //                 <CFormInput
-    //                   type="password"
-    //                   placeholder="Password"
-    //                   autoComplete="current-password"
-    //                 />
-    //               </CInputGroup>
-    //               <CRow>
-    //                 <CCol xs={6}>
-    //                   <CButton color="success" className="px-4" onClick={(e) => handleLogin(e)}>
-    //                     Login
-    //                   </CButton>
-    //                 </CCol>
-    //                 <CCol xs={6} className="text-right">
-    //                   <CButton color="link" className="px-0">
-    //                     Forgot password?
-    //                   </CButton>
-    //                 </CCol>
-    //               </CRow>
-    //             </CForm>
-    //           </CCardBody>
-    //         </CCard>
-    //       </CCol>
-    //       <CCol>
-    //         <CCard className="text-white bg-primary" style={{ background: '#2c3e50 !important' }}>
-    //           <CCardBody className="text-center">
-    //             <div>
-    //               <h2>DocuAi</h2>
-    //               <p>
-    //                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-    //                 incididunt ut labore et dolore magna aliqua.
-    //               </p>
-    //               <Link to="/register">
-    //                 <CButton color="primary" className="mt-3" active tabIndex={-1}>
-    //                   Register Now!
-    //                 </CButton>
-    //               </Link>
-    //             </div>
-    //           </CCardBody>
-    //         </CCard>
-    //       </CCol>
-    //     </CRow>
-    //   </CContainer>
-    // </div>
   )
 }
 
