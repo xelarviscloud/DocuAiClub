b0c17762adec410234410b3876eb5542bb8f00a2
HG <ghori_hitesh@yahoo.com>
2024-04-19 11:27:22 -0500

    t

--- src/routes.js
+++ src/routes.js
@@ -134,11 +134,6 @@ const routes = [
   { path: '/notifications/modals', name: 'Modals', element: Modals },
   { path: '/notifications/toasts', name: 'Toasts', element: Toasts },
   { path: '/widgets', name: 'Widgets', element: Widgets },
-  {
-    route: '/*',
-    serve: '/index.html',
-    statusCode: 200,
-  },
 ]
 
 export default routes
